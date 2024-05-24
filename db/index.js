c92aec39fe9df244acd6526419a3275f3b265ac1
Emmanuel-002 <segseyman8@gmail.com>
2024-05-24 10:29:59 +0100

    Intial Commit

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = () =>
   mongoose.connect(
-    `mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0-jnhjs.mongodb.net/test?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0.td1jgh2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
